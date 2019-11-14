832d319a93659e111a4477fa7a7cf6892d10fea3
Dylandk10 <Dylandk10@gmail.com>
2019-11-14 16:53:06 -0500

    cleaned Input

--- script.py
+++ script.py
@@ -43,6 +43,9 @@ class CharacterHandler:
         self.username_file_path = OS_file_path.get_username_file_path()
         self.character_file_path = OS_file_path.get_character_file_path()
         self.metadata_file_path = OS_file_path.get_metadata_file_path()
+        #if the chasracter exist set the pass and socre
+        self.password, self.score = self.set_password_and_score()
+        self.print_character_fields()
     #search all characters and see if the character exist
     def search_for_name(self):
         print("searching for name in character directory")
@@ -76,6 +79,23 @@ class CharacterHandler:
             character_file.close()
             name_file.close()
             return flag
+    #returns the password and score of the character
+    def set_password_and_score(self):
+        if self.search_for_name():
+            file = open(self.character_file_path + self.name + ".txt", "r")
+            name_line = file.readline().split("= ")
+            password_line = file.readline().split("= ")
+            score_line = file.readline().split("= ")
+            token_password = password_line[1].strip()
+            token_score = score_line[1].strip()
+            return token_password, token_score
+    #print the fields to show completion
+    def print_character_fields(self):
+        print("Chacter fields")
+        print("---------------------------------")
+        print("username: " + self.name)
+        print("password: " + self.password)
+        print("score: " + self.score)
     #makes a new character file
     def git_changed_make_new_file(self):
         print("Change in git status character not found")
@@ -96,6 +116,23 @@ class CharacterHandler:
         file.write("password = " + password + "\n")
         file.write("score = " + score + "\n")
         file.close()
+    #to change the password of the characters
+    def change_password(self, password):
+        self.password = password
+        self.save_charcter_file()
+    #to change the score of the character file
+    def change_score(self, score):
+        self.score = score
+        self.save_charcter_file
+    #save the character file
+    def save_charcter_file():
+        print("Change in git status character not found")
+        #file is not made so make a file and add the Data
+        file = open(self.character_file_path + self.name +".txt", "w+")
+        file.write("username = " + self.name +"\n")
+        file.write("password = " + self.password +"\n")
+        file.write("score = " + self.score + "\n")
+        file.close()
     #make a meta data file
     def make_meta_data_file(self):
         today = date.today()
@@ -111,28 +148,25 @@ class CharacterHandler:
 #made easier to use with mac/linux and windows
 class OSFileHandler:
     def __init__(self):
-        self.path = ""
+        #if windows set all paths
+        if platform == "win32":
+            self.username_path = os.getcwd() + "../../../project2/Data/UserNames.txt"
+            self.charcter_path = os.getcwd() + "../../../project2/Characters/"
+            self.meta_path = os.getcwd() + "../../../project2/Meta/"
+        #else mac and linux set paths
+        else :
+            self.username_path = "../../project2/Data/UserNames.txt"
+            self.character_path = "../../project2/Characters/"
+            self.meta_path = "../../project2/Meta/"
     #handle UserName file paths
     def get_username_file_path(self):
-        if platform == "win32":
-            self.path = os.getcwd() + "../../../project2/Data/UserNames.txt"
-        else:
-            self.path = "../../project2/Data/UserNames.txt"
-        return self.path
+        return self.username_path
     #handles getting thr character file path
     def get_character_file_path(self):
-        if platform == "win32":
-            self.path = os.getcwd() + "../../../project2/Characters/"
-        else:
-            self.path = "../../project2/Characters/"
-        return self.path
+        return self.character_path
     #return the meta data file math
     def get_metadata_file_path(self):
-        if platform == "win32":
-            self.path = os.getcwd() + "../../../project2/Meta/"
-        else:
-            self.path = "../../project2/Meta/"
-        return self.path
+        return self.meta_path
 
 #controls the menu and sub menus
 class InputHandler:
@@ -154,14 +188,26 @@ class InputHandler:
     #the menu for updating character properties
     def update_menu(self):
         #file the name in file and if not make a new
-        name = input("enter username for character file search: ")
+        name = input("Enter username for character file search: ")
         #make a character object from the name
         character_name = CharacterHandler(name)
         flag = character_name.search_for_name()
         if not flag:
-            #make a new character file and add user name to user name file
-            character_name.git_changed_make_new_file()
-            all_user_names.add_new_user(name)
+            make_character = input("make a new character file? (y/n)")
+            if make_character == "yes" pr make_character[0] == 'y':
+                #make a new character file and add user name to user name file
+                character_name.git_changed_make_new_file()
+                all_user_names.add_new_user(name)
+                print("charcter not found made new character with name " + name)
+        else:
+            field = input("Enter field to change password or score: ")
+            if field == "password" or field[0] == 'p':
+                password = input("Enter new password")
+                chacacter_name.change_password(password)
+            elif field == "score" or field[0] == 's':
+                score = input("Enter the new score")
+                character_name.change_score(score)
+        self.main_menu()
     #menu to search for character
     def search_menu(self):
         strs = input("Search for character name or print all names: ")
